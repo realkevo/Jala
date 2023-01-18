a5d55ee82cfe21047f94e8a550ef908288970d7c
Kelvin thuranira <58592608+micro-byte@users.noreply.github.com>
2023-01-18 09:17:01 +0300

    Add files via upload
    Jala taskone

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {  int a = 10; int b = 20; static int local (){ int y=80; int x = 20; int z = y+x; return z;}
+     static void Name(){
+
+        System.out.println("kelvin is my name and i have been called by this method");
+     }
+
+    public static void main(String[] args)
+    {
+        // this is jala assignment task one SINGLE LINE COMMENT
+        /*This  java comments*/
+        /* I will as well define int,boolean, char,float and double datatypes here
+        float data type is used when dealing with figures that bear decimal points e.g ksh30.60
+        float amount = 78.9;
+        boolean datatype takes the two conditions true or false
+        char-this is single character datatype
+        double-THis like float datatype reprent numbers with decimal precision
+        int-This is used to rep whole numbers
+         */
+//Local variables are created with a method while global variables are created outside the meain method and can be acceessed throughout the program
+        System.out.println("Kelvin Thuranira is my name"); 
+Name(); }
+}
